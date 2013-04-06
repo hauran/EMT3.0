0f256c5210fcadcdaf5cc1aa27f396a705a02f31
hauran <richardmai@gmail.com>
2013-04-06 00:14:43 -0400

    user to most_collected

--- db/most_collected.sql
+++ db/most_collected.sql
@@ -1,8 +1,9 @@
-select distinct m.mixId, m.mixName, m.code, m.editable, m.length, m.currentLength, count(*) c
-from userMix um, mix m
+select distinct m.mixId, m.mixName, m.code, m.editable, m.length, m.currentLength, u.userName, u.userId, count(*) c
+from userMix um, mix m, user u
 where um.mixId = m.mixId
 and m.public=0
 and m.validStatusId = 1
+and um.ownerId = u.userId
 group by mixId
 order by c desc, mixName
 {{#select.limit}}
