b6b1003ab1ef12cfacf2f2aa2a0af7d1d14886c1
hauran <richardmai@gmail.com>
2013-05-24 00:14:36 -0400

    when getting mixcard. get you tube url for image

--- db/mix_tracks.sql
+++ db/mix_tracks.sql
@@ -5,6 +5,9 @@ and p.songId = s.songId
 and p.flag=0 
 and p.validStatusId =1 
 and s.validStatusId = 1 
+{{#select.YT}}
+	and s.type=1
+{{/select.YT}}
 order by p.sort
 {{#select.limit}}
 	limit {{select.limit}}
